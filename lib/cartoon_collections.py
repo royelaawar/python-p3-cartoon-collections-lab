93ac41389a839d453d7f7229a968bd7880e24604
Roy Elaawar <roy.elaawar@gmail.com>
2023-11-13 16:44:32 -0500

    cartoon

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_list):
+    
+    i = 1
+    for name in dwarf_list:
+        print(f'{i}. {name}')
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return[f'{call.title()}!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+    
+CHEESE = ["cheddar", "gouda", "camembert"]
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+
+    for food in foods:
+        if food in CHEESE:
+            return food
+    return None
+    
